19510310858c2ad7537e3ced41d0924945f123a6
Ashish Raina <rainaash@gmail.com>
2021-09-20 07:19:24 +0530

    Adding tests and enabling Headless

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,7 +2,7 @@ module.exports = {
     launch: {
       dumpio: true,
       headless: process.env.HEADLESS === 'true',
-      args: ['--disable-infobars', '--window-size=1200,800'],
+      args: ['--disable-infobars', '--window-size=1200,800', '--headless'],
       defaultViewport: null,
     },
     browserContext: 'default',
--- src/tests/createAccount.spec.ts
+++ src/tests/createAccount.spec.ts
@@ -15,39 +15,39 @@ describe('Create Account', () => {
   });
 
   // Merge Custom test data object with default test data object
-  // it('should throw error if missing data when creating account', async () => {
-  //   var customTestData = {
-  //     title:'', customer_firstname: '', customer_lastname: '', email: '', password: "", dob: { days: "", months:"", years:""},
-  //     firstname: '', lastname: '', company: '', address1 : '', address2 : '', city : '',
-  //     id_state: '', postcode: '', id_country: '', phone : '', phone_mobile : '', alias : '', 
-  //     createAccount: {allErrors:"You must register at least one phone number.\nlastname is required.\nfirstname is required.\npasswd is required.\naddress1 is required.\ncity is required.\nThe Zip/Postal code you've entered is invalid. It must follow this format: 00000\nThis country requires you to choose a State."}
-  //   };
-  //   await accountPo.fillPersonalInformationAndCreateAccount(customTestData);
-  //   expect(await accountPo.getErrorMessage()).toContain(customTestData.createAccount.allErrors);
-  // });
+  it('should throw error if missing data when creating account', async () => {
+    var customTestData = {
+      title:'', customer_firstname: '', customer_lastname: '', email: '', password: "", dob: { days: "", months:"", years:""},
+      firstname: '', lastname: '', company: '', address1 : '', address2 : '', city : '',
+      id_state: '', postcode: '', id_country: '', phone : '', phone_mobile : '', alias : '', 
+      createAccount: {allErrors:"You must register at least one phone number.\nlastname is required.\nfirstname is required.\npasswd is required.\naddress1 is required.\ncity is required.\nThe Zip/Postal code you've entered is invalid. It must follow this format: 00000\nThis country requires you to choose a State."}
+    };
+    await accountPo.fillPersonalInformationAndCreateAccount(customTestData);
+    expect(await accountPo.getErrorMessage()).toContain(customTestData.createAccount.allErrors);
+  });
   
-  // it('create account', async () => {
-  //   var common_firstname = faker.name.firstName();
-  //   var common_lastname = faker.name.lastName();
-  //   var testData = {
-  //     title:["Mr","Mrs"][Math.floor(Math.random() * 2)].toString(),
-  //     customer_firstname: common_firstname,
-  //     customer_lastname: common_lastname,
-  //     email: faker.internet.email(),
-  //     password: faker.internet.password(),
-  //     dob: {
-  //       days: faker.datatype.number({'min': 1, 'max': 28 }).toString(),
-  //       months: faker.datatype.number({'min': 1, 'max': 12 }).toString(),
-  //       years: faker.datatype.number({'min': 1900, 'max': 2020 }).toString()
-  //     },
-  //     firstname: common_firstname, lastname: common_lastname, company: faker.company.companyName(), 
-  //     address1 : faker.address.streetAddress(), address2 : '', city : faker.address.city(),
-  //     id_state: faker.address.stateAbbr(), postcode:'00000', id_country: faker.address.countryCode(), 
-  //     phone : '', phone_mobile : faker.phone.phoneNumber('0165#######'), alias : '', 
-  //     createAccount: {allErrors:""}
-  //   }
-  //   await accountPo.fillPersonalInformationAndCreateAccount(testData);
-  //   expect(await myAccountPo.isMyAccountPage()).toBe(true);
-  // });
+  it('create account', async () => {
+    var common_firstname = faker.name.firstName();
+    var common_lastname = faker.name.lastName();
+    var testData = {
+      title:["Mr","Mrs"][Math.floor(Math.random() * 2)].toString(),
+      customer_firstname: common_firstname,
+      customer_lastname: common_lastname,
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      dob: {
+        days: faker.datatype.number({'min': 1, 'max': 28 }).toString(),
+        months: faker.datatype.number({'min': 1, 'max': 12 }).toString(),
+        years: faker.datatype.number({'min': 1900, 'max': 2020 }).toString()
+      },
+      firstname: common_firstname, lastname: common_lastname, company: faker.company.companyName(), 
+      address1 : faker.address.streetAddress(), address2 : '', city : faker.address.city(),
+      id_state: faker.address.stateAbbr(), postcode:'00000', id_country: faker.address.countryCode(), 
+      phone : '', phone_mobile : faker.phone.phoneNumber('0165#######'), alias : '', 
+      createAccount: {allErrors:""}
+    }
+    await accountPo.fillPersonalInformationAndCreateAccount(testData);
+    expect(await myAccountPo.isMyAccountPage()).toBe(true);
+  });
 
 });
