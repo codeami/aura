b00910b8958f747a6ce5c68457c8ffc3898e8925
Ashish Raina <rainaash@gmail.com>
2021-09-21 01:49:43 +0530

    Added headless execution scripts

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,8 +2,10 @@ module.exports = {
     launch: {
       dumpio: true,
       headless: process.env.HEADLESS === 'true',
-      args: ['--disable-infobars', '--window-size=1200,800', '--headless'],
+      args: ['--disable-infobars', '--window-size=1200,800'],
       defaultViewport: null,
     },
     browserContext: 'default',
   };
+
+  // $ npm run test -- --global.puppeteerConfig.launch.args ["--headless"]
